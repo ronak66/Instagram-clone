1670cf9e6eb8bab036b9ebdbdfc12b39e87560ab
Ronak Doshi <ronakvipul.doshi@iiitb.org>
2018-06-06 16:35:00 +0530

    Adding upload image feature

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.10'
 gem 'devise'
+gem "paperclip", "~> 6.0.0"
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 gem 'bootstrap-sass', '~> 3.3.7'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,30 @@
 class PostsController < ApplicationController
   def new
+  	@post = Post.new
   end
 
   def index
+  	@post = Post.all
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
+
+  def create
+  	@post = Post.new(permit_post)
+  	if @post.save
+  		flash[:success] = "Success"
+  		redirect_to post_path(@post)
+  	else
+  		flash[:error] = @post.error.full_messages
+  		redirect_to new_post_path
+  	end
+  end
+
+
+  private
+  	def permit_post
+  		params.require(:post).permit(:image,:description)
+  	end
 end
